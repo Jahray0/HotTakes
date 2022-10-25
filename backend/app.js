1a1e26a1dc66cd501233502dd7704ea02cd82085
Jahray0 <jaegle.maxime@gmail.com>
2022-10-25 13:42:04 +0200

    v1.03 mongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://user2:Piquante123@cluster0.k3odr5k.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+  
 
 app.use((req, res, next) => {
     console.log('Requete reçu !');
