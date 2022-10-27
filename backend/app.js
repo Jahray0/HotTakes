79c98533fb137165dd84c1fdd7fe906ddfea702e
Jahray0 <jaegle.maxime@gmail.com>
2022-10-27 10:41:36 +0200

    v1.05 login ok

--- backend/app.js
+++ backend/app.js
@@ -1,33 +1,38 @@
-const express = require('express');
+const express = require("express");
 const mongoose = require('mongoose');
+const path = require('path');
 
-const app = express();
+// const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
+//Logique pour se connecter a mongo DB
 mongoose.connect('mongodb+srv://user2:Piquante123@cluster0.k3odr5k.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-  
+  const app = express();
 
+app.use(express.json()); //intercept les requet JSON
+
+//CORS
 app.use((req, res, next) => {
-    console.log('Requete reçu !');
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
 });
 
-app.use((req, res, next) => {
-    res.status(201);
-    next();
-});//change le status
+// app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
-app.use((req, res, next) => {
-    res.json({ message:'Votre requete a bien été reçu ! ' });
-    next();
-});//envoi la réponse
-
-app.use((req, res) => {
-    console.log('Réponse envoyée avec succes');
-});
 
 module.exports = app;
