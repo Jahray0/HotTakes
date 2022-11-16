48fa73a696d1ea345c99a6f15cd5ae146361dd7e
Jahray0 <jaegle.maxime@gmail.com>
2022-11-16 18:07:49 +0100

    v9.02

--- backend/middleware/password.js
+++ backend/middleware/password.js
@@ -3,7 +3,7 @@ const passwordModel = require("../models/password");
 module.exports = (req, res, next) => {
   //Si aucun mot de passe n'est renseigné
   if (req.body.password == null) {
-    res.status(400).json({ error: `Veuillez entrer un mot de passe.` });
+    res.status(400).json({ error: `Veuillez entrer un nom d'utilisateur et un mot de passe.` });
     //sinon si le mot de passe ne remplis pas les critères
   } else if (!passwordModel.validate(req.body.password)) {
     res.status(400).json({ error: `Veuillez entrer un mot de passe avec au moins 8 caracteres, une majuscule, une mminuscule et au moins 2 chiffres .` });
